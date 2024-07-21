cd24948b0e232005ce787de28f887d1a53eaf5b1
Thiago Santos <thiago.lrsnts@gmail.com>
2024-07-21 00:25:46 -0300

    feat: add mask to the search input

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -4,16 +4,34 @@ import { useHistory } from "react-router-dom";
 import { IconButton, Button, TextField } from "~/components";
 import routes from "~/router/routes";
 import * as S from "./styles";
-export const SearchBar = () => {
+import { useState } from "react";
+import { masks } from "~/utils";
+
+type SearchBarProps = {
+  onChange: (value: string) => void;
+};
+
+export const SearchBar = ({ onChange }: SearchBarProps) => {
+  const [value, setValue] = useState("");
+
   const history = useHistory();
 
   const goToNewAdmissionPage = () => {
     history.push(routes.newUser);
   };
-  
+
+  const handleChange = (value: string) => {
+    setValue(value);
+    onChange(value);
+  };
+
   return (
     <S.Container>
-      <TextField  placeholder="Digite um CPF válido" />
+      <TextField
+        placeholder="Digite um CPF válido"
+        value={masks.cpf(value)}
+        onChange={(e) => handleChange(e.target.value)}
+      />
       <S.Actions>
         <IconButton aria-label="refetch">
           <HiRefresh />
